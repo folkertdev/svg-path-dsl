acfc7ba2f5b4a482969bf0bef9d78142943173fc
Folkert de Vries <folkert@folkertdev.nl>
2016-09-11 20:26:11 +0200

    implement fmap and propagate. helpers for higher-order diagrams

--- src/Svg/Path/Internal.elm
+++ src/Svg/Path/Internal.elm
@@ -10,6 +10,10 @@ type alias Point =
     ( Float, Float )
 
 
+point x y =
+    ( x, y )
+
+
 {-| the T, t, S and s commands
 -}
 type CurveContinuation
@@ -254,3 +258,254 @@ roundToAtMost doRound =
 formatPointWithPrecision : Maybe Int -> Point -> String
 formatPointWithPrecision dp ( x, y ) =
     roundToAtMost dp x ++ "," ++ roundToAtMost dp y
+
+
+translate : Point -> Instruction -> Instruction
+translate ( dx, dy ) instruction =
+    map (\( x, y ) -> ( x + dx, y + dy )) instruction
+
+
+mapPointsCurveCont f curveCont =
+    case curveCont of
+        QuadAbsolute p ->
+            QuadAbsolute (f p)
+
+        QuadRelative p ->
+            QuadRelative (f p)
+
+        CubiAbsolute c p ->
+            CubiAbsolute (f c) (f p)
+
+        CubiRelative c p ->
+            CubiRelative (f c) (f p)
+
+
+addPoint ( x1, y1 ) ( x2, y2 ) =
+    ( x1 + x2, y1 + y2 )
+
+
+last : List a -> Maybe a
+last list =
+    case list of
+        [] ->
+            Nothing
+
+        [ x ] ->
+            Just x
+
+        x :: xs ->
+            last xs
+
+
+type alias DrawState =
+    { start : Point, current : Point, instructions : List Instruction }
+
+
+fromReversed { start, current, instructions } =
+    DrawState start current (List.reverse instructions)
+
+
+{-| A draw state where the instructions are reversed
+-}
+type alias ReversedDrawState =
+    { start : Point, current : Point, instructions : List Instruction }
+
+
+run : List Instruction -> ReversedDrawState -> ReversedDrawState
+run list state =
+    let
+        helper elem accum =
+            let
+                newState =
+                    propagate elem accum
+            in
+                { newState | instructions = elem :: newState.instructions }
+    in
+        List.foldl helper state list
+
+
+propagate : Instruction -> { a | start : Point, current : Point } -> { a | start : Point, current : Point }
+propagate instruction ({ start, current } as state) =
+    let
+        ( cx, cy ) =
+            current
+
+        resultContinuation continuation state =
+            case continuation of
+                QuadAbsolute p ->
+                    { state | current = p }
+
+                QuadRelative p ->
+                    { state | current = addPoint state.current p }
+
+                CubiAbsolute _ p ->
+                    { state | current = p }
+
+                CubiRelative _ p ->
+                    { state | current = addPoint state.current p }
+    in
+        case instruction of
+            MoveAbsolute p ->
+                { state | current = p }
+
+            MoveRelative p ->
+                { state | current = addPoint p current }
+
+            MoveAbsoluteMany ps ->
+                case last ps of
+                    Nothing ->
+                        state
+
+                    Just p ->
+                        { state | current = p }
+
+            MoveRelativeMany ps ->
+                case last ps of
+                    Nothing ->
+                        state
+
+                    Just p ->
+                        { state | current = addPoint current p }
+
+            LineAbsolute p ->
+                { state | current = p }
+
+            LineRelative p ->
+                { state | current = addPoint current p }
+
+            VerticalAbsolute y ->
+                { state | current = point cx y }
+
+            VerticalRelative y ->
+                { state | current = point cx (cy + y) }
+
+            HorizontalAbsolute x ->
+                { state | current = point x cy }
+
+            HorizontalRelative x ->
+                { state | current = point (cx + x) cy }
+
+            LineAbsoluteMany ps ->
+                case last ps of
+                    Nothing ->
+                        state
+
+                    Just p ->
+                        { state | current = p }
+
+            LineRelativeMany ps ->
+                List.foldl (\rel newState -> { newState | current = addPoint rel newState.current }) state ps
+
+            ArcTo radii xAngleRotation flags p ->
+                { state | current = p }
+
+            ArcBy radii xAngleRotation flags p ->
+                { state | current = addPoint current p }
+
+            QuadraticAbsolute control p ->
+                { state | current = p }
+
+            QuadraticRelative control p ->
+                { state | current = addPoint current p }
+
+            CubicAbsolute c1 c2 p ->
+                { state | current = p }
+
+            CubicRelative c1 c2 p ->
+                { state | current = addPoint current p }
+
+            QuadraticAbsoluteMany control goal continuations ->
+                List.foldl resultContinuation { state | current = goal } continuations
+
+            QuadraticRelativeMany control goal continuations ->
+                List.foldl resultContinuation { state | current = addPoint current goal } continuations
+
+            CubicAbsoluteMany c1 c2 goal continuations ->
+                List.foldl resultContinuation { state | current = goal } continuations
+
+            CubicRelativeMany c1 c2 goal continuations ->
+                List.foldl resultContinuation { state | current = addPoint current goal } continuations
+
+            ClosePath ->
+                { state | current = start }
+
+
+{-| Applies a function to all wrapped coordinates.
+-}
+map f instruction =
+    let
+        fX =
+            (\x -> fst (f ( x, 0 )))
+
+        fY =
+            (\y -> snd (f ( 0, y )))
+    in
+        case instruction of
+            MoveAbsolute p ->
+                MoveAbsolute (f p)
+
+            MoveRelative p ->
+                MoveRelative (f p)
+
+            MoveAbsoluteMany p ->
+                MoveAbsoluteMany (List.map f p)
+
+            MoveRelativeMany p ->
+                MoveRelativeMany (List.map f p)
+
+            LineAbsolute p ->
+                LineAbsolute (f p)
+
+            LineRelative p ->
+                LineRelative (f p)
+
+            VerticalAbsolute y ->
+                VerticalAbsolute (fY y)
+
+            VerticalRelative y ->
+                VerticalRelative (fY y)
+
+            HorizontalAbsolute x ->
+                HorizontalAbsolute (fX x)
+
+            HorizontalRelative x ->
+                HorizontalRelative (fX x)
+
+            LineAbsoluteMany p ->
+                LineAbsoluteMany (List.map f p)
+
+            LineRelativeMany p ->
+                LineRelativeMany (List.map f p)
+
+            ArcTo radii xAngleRotation flags p ->
+                ArcTo radii xAngleRotation flags (f p)
+
+            ArcBy radii xAngleRotation flags p ->
+                ArcBy radii xAngleRotation flags (f p)
+
+            QuadraticAbsolute control goal ->
+                QuadraticAbsolute (f control) (f goal)
+
+            QuadraticRelative control goal ->
+                QuadraticRelative (f control) (f goal)
+
+            CubicAbsolute c1 c2 point ->
+                CubicAbsolute (f c1) (f c2) (f point)
+
+            CubicRelative c1 c2 point ->
+                CubicRelative (f c1) (f c2) (f point)
+
+            QuadraticAbsoluteMany control goal continuations ->
+                QuadraticAbsoluteMany (f control) (f goal) (List.map (mapPointsCurveCont f) continuations)
+
+            QuadraticRelativeMany control goal continuations ->
+                QuadraticRelativeMany (f control) (f goal) (List.map (mapPointsCurveCont f) continuations)
+
+            CubicAbsoluteMany c1 c2 goal continuations ->
+                CubicAbsoluteMany (f c1) (f c2) (f goal) (List.map (mapPointsCurveCont f) continuations)
+
+            CubicRelativeMany c1 c2 goal continuations ->
+                CubicRelativeMany (f c1) (f c2) (f goal) (List.map (mapPointsCurveCont f) continuations)
+
+            ClosePath ->
+                ClosePath
